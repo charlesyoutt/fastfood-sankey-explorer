d907ba246196b638cf4a79f521ee8f3a859c5fff
charlesyoutt <charlieyoutt@gmail.com>
2026-01-28 15:47:50 -0500

    Initial commit

---
+++ fast_api.py
@@ -0,0 +1,102 @@
+"""
+File: fast_api.py
+
+Description:
+Provides an API to process and visualize fast food nutritional data.
+It allows users to filter menu items based on restaurant selection and fat content
+and then generates a Sankey diagram to visualize food item distributions
+"""
+
+import panel as pn
+import pandas as pd
+import sankey2 as sk
+
+
+class FASTAPI:
+    """
+    An API to load, process, and filter fast food data
+    """
+
+    def load_fast(self, filename):
+        """
+        Loads fast food data from a CSV file
+
+        :param filename: The name of the CSV file containing fast food data
+        :return: None
+        """
+        self.fast = pd.read_csv(filename)
+
+    def get_restaurants(self):
+        """
+        Gets a list of unique restaurants from the dataset
+
+        :return: A list of restaurant names in lowercase.
+        """
+        fastfood = self.fast
+
+        # convert all restaurant names to lowercase
+        fastfood['restaurant'] = fastfood['restaurant'].str.lower()
+
+        # get the unique restaurant names and return as a list
+        fastfood_restaurants = fastfood['restaurant'].unique().tolist()
+        return fastfood_restaurants
+
+    def extract_local_network(self, restaurant, max_fat):
+        """
+        Filters fast food items based on the restaurant selection and fat content
+
+        :param restaurant: The restaurant name
+        :param max_fat: The maximum fat content allowed
+        :return: A filtered DataFrame containing relevant fast food data
+        """
+
+        fast = pd.read_csv('fastfood.csv').copy()
+
+        # round calories and fat content into bins
+        fast['calories_100'] = fast['calories'] // 100 * 100
+        fast['total_fat_10'] = fast['total_fat'] // 5 * 5
+
+        # filter columns
+        fast_filter = fast[['restaurant', 'calories_100', 'total_fat_10']].copy()
+        fast_filter['restaurant'] = fast_filter['restaurant'].str.lower()
+
+        # make sure max_fat is correctly interpreted
+        max_fat_value = max_fat.value if isinstance(max_fat, pn.widgets.IntSlider) else max_fat
+
+        # filter to include only items with total fat within the allowed limit
+        fast_filter = fast_filter[fast_filter['total_fat_10'] <= max_fat_value]
+
+        # aggregate the number of food items per restaurant and calorie range
+        fast_filter = fast_filter.groupby(['restaurant', 'calories_100']).size().reset_index(name="num_foods")
+
+        # sort the data to show most common food categories first
+        fast_filter.sort_values('num_foods', ascending=False, inplace=True)
+
+        # get the data specific to the selected restaurant
+        fast_rest = fast_filter[fast_filter.restaurant == restaurant]
+
+        # get only items that match the calorie bins of the selected restaurant and convert calories column to strings
+        local = fast[fast.calories_100.isin(fast_rest.calories_100)].copy()
+        local['calories_100'] = local['calories_100'].astype(str)
+
+        return local
+
+
+def main():
+
+    # initialize the API
+    fastapi = FASTAPI()
+    fastapi.load_fast('fastfood.csv')
+
+    # search parameters
+    restaurant = 'mcdonalds'
+    max_fat = 30
+
+    # filter fast data to build the dataframe for sankey visualization
+    local = fastapi.extract_local_network(restaurant, max_fat)
+
+    # generate sankey diagram
+    sk.show_sankey(local, 'restaurant', 'calories_100', vals='total_fat_10')
+
+if __name__ == '__main__':
+    main()
---
+++ fastexplorer.py
@@ -0,0 +1,111 @@
+"""
+File: fastexplorer.py
+
+Description:
+This script creates an interactive web application using Panel and FastAPI to explore fast food
+nutritional data. Users can filter menu items by restaurant and fat content, visualize relationships
+using a Sankey diagram. They then can interact with dynamically generated tables and plots through a dashboard.
+"""
+
+import panel as pn
+from fast_api import FASTAPI
+import sankey2 as sk
+
+pn.extension()
+
+# initialize fastAPI
+api = FASTAPI()
+api.load_fast("fastfood.csv")
+
+# search Widgets
+restaurant = pn.widgets.Select(name="Restaurant", options=api.get_restaurants(), value='mcdonalds')
+max_fat = pn.widgets.IntSlider(name="Max Fat", start=0, end=150, step=5, value=15)
+
+# plot the widgets
+width = pn.widgets.IntSlider(name="Width", start=250, end=2000, step=250, value=1000)
+height = pn.widgets.IntSlider(name="Height", start=200, end=2500, step=100, value=1000)
+
+# sankey diagram colors
+color_options = {
+    "Default (Black)": "black",
+    "Vibrant Red": "red",
+    "Cool Blue": "blue",
+    "Nature Green": "green"
+}
+color_picker = pn.widgets.Select(name="Sankey Color", options=list(color_options.keys()), value="Default (Black)")
+
+# callback functions
+def get_catalog(restaurant, max_fat):
+    """
+    Creates a table showing food items from a specific restaurant and then
+    filters by a maximum fat limit
+
+    :param restaurant: The name of the restaurant to pull data from
+    :param max_fat: The highest amount of fat in grams allowed for the food items
+    :return: A table displaying the filtered food items
+    """
+    global local
+    local = api.extract_local_network(restaurant, max_fat)
+    local = local.reset_index(drop=True)
+    table = pn.widgets.Tabulator(local, selectable=False, page_size=100)
+    return table
+
+def get_plot(restaurant, max_fat, width, height, color_picker):
+    """
+    Creates a Sankey diagram to visualize how different restaurants' menu items
+    contribute to calorie distribution, based on fat content
+
+    :param restaurant: The restaurant being analyzed
+    :param max_fat: The max fat content in grams for food items included in the chart
+    :param width: The width of the diagram
+    :param height: The height of the diagram
+    :param color_picker: The color choice for the Sankey lines
+    :return: A Sankey diagram showing calorie flow from restaurant items
+    """
+    return sk.make_sankey(local, "restaurant", "calories_100", vals="total_fat_10", width=width,
+                          height=height, line_color=color_options[color_picker])
+
+# callback bindings
+catalog = pn.bind(get_catalog, restaurant, max_fat)
+plot = pn.bind(get_plot, restaurant, max_fat, width, height, color_picker)
+
+card_width = 350
+
+# make the search card
+search_card = pn.Card(
+    pn.Column(
+        restaurant,
+        max_fat,
+    ),
+    title="Search", width=card_width, collapsed=False
+)
+
+# make the plot card
+plot_card = pn.Card(
+    pn.Column(
+        width,
+        height,
+        color_picker
+    ),
+    title="Plot", width=card_width, collapsed=True
+)
+
+# make layout
+layout = pn.template.FastListTemplate(
+    title="Food Explorer",
+    sidebar=[
+        search_card,
+        plot_card,
+    ],
+    main=[
+        pn.Tabs(
+            ("Associations", catalog),
+            ("Network", plot),
+            active=1
+        )
+    ],
+    header_background='#1e3a8a',
+    theme='default'
+).servable()
+
+layout.show()
